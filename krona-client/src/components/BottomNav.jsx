9bb98ac87919e517534a401d9430c76e3938cda4
Ali Al Kadri <ali_kadri2016@hotmail.com>
2026-03-16 14:57:05 +0100

    dashboard created, base

---
+++ krona-client/src/components/BottomNav.jsx
@@ -0,0 +1,16 @@
+function BottomNav() {
+
+    return (
+
+
+        <nav className="bottom-nav">
+
+            <span>Hjem</span>
+            <span>Sparing</span>
+            <span>?</span>
+            <span>instillinger</span>
+        </nav>
+    )
+}
+
+export default BottomNav
---
+++ krona-client/src/components/TopBar.jsx
@@ -0,0 +1,14 @@
+function TopBar() {
+
+    return (
+
+        <div className="topbar">
+
+            <span className="toppbar-menu">☰</span>
+            <span className="toppbar-title">Krona</span>
+            <span className="toppbar-menu">profil</span>
+        </div>
+    )
+}
+
+export default TopBar
